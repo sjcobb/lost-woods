0fb822e89fc2975e7a960e854c08a93f8081ee78
Steven Cobb <stevecobb91@gmail.com>
2017-01-29 01:09:14 -0500

    inventory btn control styles

--- js/inventory.js
+++ js/inventory.js
@@ -1,9 +1,18 @@
 /*
 *** INVENTORY JS ***
 */
-console.log("inventory.js called");
-
+//console.log("inventory.js called");
 var hasKey = false;
+
+var itemsDiv = document.createElement("div");
+itemsDiv.setAttribute("id", "itemsDiv");
+var itemsStyle = "position:absolute; top:20px; right:0; width:50%; height:100px; text-align:center;"
+itemsDiv.setAttribute("style", itemsStyle);
+document.body.appendChild(itemsDiv);
+
+itemsHTML = '<div class="btn green" style="float:left; width:55px; height:55px; margin:0 25px 0 0; background:green; border-radius:50%; cursor:pointer;"></div><div class="btn blue" style="float:left; width:55px; height:55px; background:blue; border-radius:50%; cursor:pointer;"></div>';
+document.getElementById("itemsDiv").innerHTML = itemsHTML;
+
 
 function updateInventory() {
 	console.log("updateInventory() called");
@@ -13,8 +22,8 @@ function updateInventory() {
 	keyHTML = '<div class="key" style="display:block; width:30px; height:30px; margin:0 0 20px 0; background:#fff; position:relative; top:10px; left:10px;"><div class="circle" style="display:block; margin: 0 auto; background:#000; width:10px; height:10px; position:absolute; top:10px; bottom:0; left:0;right:0; border-radius:50%;"><div class="handle" style="display:block; width:50px; height:5px; position:relative; left:20px; background:#fff;"><div class="end" style="display:block; width:5px; height:15px; position:absolute; top:0; right:15px; background:#fff;"></div><div class="end2" style="display:block; width:5px; height:15px; position:absolute; top:0; right:5px; background:#fff;"></div></div></div></div>'+keyNum;
 	var keyDiv = document.createElement("div");
 	keyDiv.setAttribute("id", "keyDiv");
-	var style = "position:absolute; top:0; left:0; width:100px; height:100px; padding:10px; background:#000; text-align:center; font-size:20px;"
-	keyDiv.setAttribute("style", style);
+	var keyStyle = "position:absolute; top:0; left:0; width:100px; height:100px; padding:10px; background:#000; text-align:center; font-size:20px;"
+	keyDiv.setAttribute("style", keyStyle);
 
 	document.body.appendChild(keyDiv);
 
