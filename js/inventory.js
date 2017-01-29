c28464f6f94a5cddd9f8404f007b6ffc01e1a309
Steven Cobb <stevecobb91@gmail.com>
2017-01-28 23:46:00 -0500

    start inventory key pickup

---
+++ js/inventory.js
@@ -0,0 +1,23 @@
+/*
+*** INVENTORY JS ***
+*/
+var hasKey = false;
+
+function updateInventory() {
+	hasKey = true;
+	keyNum = 1;
+
+	keyHTML = '<div class="key" style="display:block; width:30px; height:30px; margin:0 0 20px 0; background:#fff; position:relative; top:10px; left:10px;"><div class="circle" style="display:block; margin: 0 auto; background:#000; width:10px; height:10px; position:absolute; top:10px; bottom:0; left:0;right:0; border-radius:50%;"><div class="handle" style="display:block; width:50px; height:5px; position:relative; left:20px; background:#fff;"><div class="end" style="display:block; width:5px; height:15px; position:absolute; top:0; right:15px; background:#fff;"></div><div class="end2" style="display:block; width:5px; height:15px; position:absolute; top:0; right:5px; background:#fff;"></div></div></div></div>'+keyNum;
+	var keyDiv = document.createElement("div");
+	keyDiv.setAttribute("id", "keyDiv");
+	var style = "position:absolute; top:0; left:0; width:100px; height:100px; padding:10px; background:#000; text-align:center; font-size:20px;"
+	keyDiv.setAttribute("style", style);
+
+	document.body.appendChild(keyDiv);
+
+	document.getElementById("keyDiv").innerHTML = keyHTML;
+	//console.log(document.getElementById("keyDiv").innerHTML);
+
+
+	//parent.location = "?hasKey=true";
+}
--- js/vr-controls.js
+++ js/vr-controls.js
@@ -63,17 +63,27 @@ THREE.FirstPersonVRControls = function ( camera, scene ) {
 
     //console.log(camera.position);
     if (camera.position.z > 12) { //bottom
-      window.location = "https://sjcobb.github.io/fire-temple/";
+      if (hasKey == true) {
+        //window.location = "https://sjcobb.github.io/fire-temple/?hasKey=true";
+        window.location = "http://fire.dev/?hasKey=true";
+      } else {
+        window.location = "https://sjcobb.github.io/fire-temple/";
+      }
     }
     if (camera.position.x < -12) { //left
-      window.location = "https://ybinstock.github.io/BatCave/";
+      //window.location = "https://ybinstock.github.io/BatCave/";
       //window.location = "https://ybinstock.github.io/Platos-Cave/";
       //window.location = "http://ybinstock.github.io/carmensandiego_360/";
       //window.location = "https://ybinstock.github.io/starstuff/";
-      //window.location = "https://github.com/sjcobb";
     }
     if (camera.position.x > 12) { //right
-      window.location = "https://aframe.io/a-blast";
+      //window.location = "https://aframe.io/a-blast";
+    }
+    if (camera.position.z < -7) { //top
+      console.log("key picked up");
+      //console.log(parent.location);
+      //console.log(document.location);
+      updateInventory();
     }
     if (camera.position.z < -12 || camera.position.z > 12 || camera.position.x > 12 || camera.position.x < -12) {
       this.resetHero = true;
