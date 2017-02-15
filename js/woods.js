4358589997fd7e79ba90d6685421c13440627e3b
Steven Cobb <stevecobb91@gmail.com>
2017-02-14 20:08:57 -0500

    add key condition to ice cavern

--- js/woods.js
+++ js/woods.js
@@ -3,16 +3,18 @@ function checkWoods(object) {
   //console.log(object);
   //console.log(camera.position);
   if (camera.position.z > 12) { //bottom
-    window.location = "https://sjcobb.github.io/ice-cavern/";
+    if (hasKey == true) {
+      window.location = "https://sjcobb.github.io/ice-cavern/?hasKey=true";
+    } else {
+      window.location = "https://sjcobb.github.io/ice-cavern/";
+    }
   }
   if (camera.position.x < -12) { //left
     if (hasKey == true) {
       window.location = "https://sjcobb.github.io/fire-temple/?hasKey=true";
-      //window.location = "http://fire.dev/?hasKey=true";
     } else {
       window.location = "https://sjcobb.github.io/fire-temple/";
     }
-
   }
   if (camera.position.x > 12) { //right
     //window.location = "https://aframe.io/a-blast";
